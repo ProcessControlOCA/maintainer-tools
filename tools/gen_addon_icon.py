dbcf6ec3c4904bf47670a7b42868f11c86440c27
David Ramia <david.ramia@processcontrol.es>
2022-12-31 18:29:46 +0100

    fix icons

--- tools/gen_addon_icon.py
+++ tools/gen_addon_icon.py
@@ -6,8 +6,6 @@ from .manifest import read_manifest, NoManifestFound
 
 ICONS_DIR = os.path.join('static', 'description')
 
-ICON_TYPES = ['png', 'svg']
-
 
 @click.command()
 @click.option("--addons-dir", default=".")
@@ -16,19 +14,18 @@ def main(addons_dir, icon_file):
     """ Put default ProcessControl icon.
     Do nothing if the icon already exists in ICONS_DIR, otherwise put the default icon.
     """
-    icon_file = icon_file or os.path.join(os.path.dirname(__file__), 'icon.svg')
+    icon_file = icon_file or os.path.join(os.path.dirname(__file__), 'icon.png')
     for addon_dir in os.listdir(addons_dir):
         try:
             read_manifest(addon_dir)
         except NoManifestFound:
             continue
         icon_dir = os.path.join(addon_dir, ICONS_DIR)
-        exist = [os.path.exists(os.path.join(icon_dir, 'icon.%s' % t)) for t in ICON_TYPES]
-        if any(exist):
+        if os.path.exists(os.path.join(icon_dir, 'icon.png')):
             continue
         if not os.path.exists(icon_dir):
             os.makedirs(icon_dir)
-        shutil.copyfile(icon_file, os.path.join(icon_dir, 'icon.svg'))
+        shutil.copyfile(icon_file, os.path.join(icon_dir, 'icon.png'))
 
 
 if __name__ == '__main__':
